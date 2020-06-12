4a719ddb396d87f2ae2ee218899d51bc5013bfb5
mhtaldmr <mht.alidemir@gmail.com>
2020-06-12 23:55:17 +0300

    Updates

---
+++ precache-manifest.2ddfd66cea85925ec2fbd009008e6106.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a5030e8498d4448a7c5f0731472d25",
+    "url": "/react-keeper/index.html"
+  },
+  {
+    "revision": "6a248ef4afd1d39244a4",
+    "url": "/react-keeper/static/js/2.6f54dbb8.chunk.js"
+  },
+  {
+    "revision": "c62847e5163317129b0b",
+    "url": "/react-keeper/static/js/main.5f5de62b.chunk.js"
+  },
+  {
+    "revision": "d235f4c99f9e143b89ef",
+    "url": "/react-keeper/static/js/runtime-main.da64f904.js"
+  }
+]);
